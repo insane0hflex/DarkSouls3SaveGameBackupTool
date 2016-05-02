e97af1c22b4f1a3f25b7d8363b5f2807c6a37479
insane0hflex <insane0hflex@users.noreply.github.com>
2016-05-02 11:49:41 -0800

    UI updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace DarkSouls3SaveGameBackupTool
     public partial class MainWindow : Window
     {
 
-        //Save game folder file path/location
+        //Dark souls 3 Save game folder file path/location
         private string saveGameLocation = ""; 
 
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
@@ -53,7 +53,11 @@ namespace DarkSouls3SaveGameBackupTool
             }
             catch (Exception ex)
             {
-                CustomErrorMessageBox("Error! Could not find the DarkSoulsIII folder. Looked for the DarkSoulsIII folder in: " + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+                string errorMessage = "Error!Could not find the DarkSoulsIII folder.Looked for the DarkSoulsIII folder in: "
+                                    + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                                    + Environment.NewLine;
+
+                CustomErrorMessageBox(errorMessage);
                 CustomErrorMessageBox(ex.ToString());
                 txtBox_darkSouls3SaveGameLocation.Text = "Error!";
             }
@@ -62,11 +66,12 @@ namespace DarkSouls3SaveGameBackupTool
 
 
         /// <summary>
-        /// 
+        /// Start creating back ups of the Dark Souls 3 save
+        /// timeInterval setting is used for the dispatchTimer Tick interval
         /// </summary>
         private void btn_enableBackUpProcess_Click(object sender, RoutedEventArgs e)
         {
-            txtBox_log.AppendText("Starting backup process." + Environment.NewLine);
+            txtBox_log.AppendText("Starting backup process. " + DateTime.Now.ToString() + Environment.NewLine);
             txtBox_log.AppendText("Creating a backup every " + GetTimeIntervalValue() + " minutes." + Environment.NewLine);
 
 
@@ -78,6 +83,9 @@ namespace DarkSouls3SaveGameBackupTool
 
             btn_startBackUpProcess.IsEnabled = false;
             btn_endBackUpProcess.IsEnabled = true;
+            txtBox_backupInterval.IsEnabled = false;
+
+            txtBox_log.ScrollToEnd();
         }
 
 
@@ -86,21 +94,22 @@ namespace DarkSouls3SaveGameBackupTool
         /// </summary>
         private void btn_endBackUpProcess_Click(object sender, RoutedEventArgs e)
         {
-            txtBox_log.AppendText(Environment.NewLine + "Stopped backup process..." + Environment.NewLine);
-            txtBox_log.AppendText("-----------------------------------------------" + Environment.NewLine);
+            txtBox_log.AppendText(Environment.NewLine + "Stopped backup process... " + DateTime.Now.ToString() + Environment.NewLine);
+            txtBox_log.AppendText("-----------------------------------------------" + Environment.NewLine + Environment.NewLine);
 
 
             dispatcherTimer.Stop();
 
-            //???? need to find a way to stop it from duplicating
             btn_startBackUpProcess.IsEnabled = true;
             btn_endBackUpProcess.IsEnabled = false;
+            txtBox_backupInterval.IsEnabled = true;
 
+            txtBox_log.ScrollToEnd();
         }
 
 
         /// <summary>
-        /// 
+        /// Tick event that is triggered every timeInterval. Actual implementation to copy the game save to create the backup copy
         /// </summary>
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
@@ -117,7 +126,6 @@ namespace DarkSouls3SaveGameBackupTool
                 File.Copy(saveGameLocation + "DS30000.sl2", saveGameLocation + dateOfBackupForFileName + "__DS30000.sl2.bak");
 
                 txtBox_log.ScrollToEnd();
-
             }
             catch (Exception ex)
             {
@@ -233,8 +241,10 @@ namespace DarkSouls3SaveGameBackupTool
         }
 
 
-
-        private void SaveTimeIntervalSetting()
+        /// <summary>
+        /// Set/Save the Time Interval for saving from the UI to the app.config file.
+        /// </summary>
+        private void SaveTimeIntervalAppSetting()
         {
             try
             {
@@ -257,7 +267,10 @@ namespace DarkSouls3SaveGameBackupTool
         }
 
 
-
+        /// <summary>
+        /// Get the TimeInterval app.config setting
+        /// Also set the txtBox_backupInterval Text with the TimeInterval app.config setting.
+        /// </summary>
         private void GetTimeIntervalAppSetting()
         {
             try
@@ -277,10 +290,12 @@ namespace DarkSouls3SaveGameBackupTool
         }
 
 
-
+        /// <summary>
+        /// Save the TimeInterval setting to the app.config file
+        /// </summary>
         private void btn_saveTimeInterval_Click(object sender, RoutedEventArgs e)
         {
-            SaveTimeIntervalSetting();
+            SaveTimeIntervalAppSetting();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,21 +1,38 @@
 # DarkSouls3SaveGameBackupTool
 
-A program that creates backups of save games of Dark Souls 3
+A program that creates backups of save games of Dark Souls 3.
 
-[Download the latest release - v1](https://github.com/insane0hflex/DarkSouls3SaveGameBackupTool/blob/master/Releases/DarkSouls3SaveGameBackupTool.exe?raw=true)
+Back ups are created in the following format (hours in 24HR military time format):
 
+`Month_Day_Year_Hour_Minute__DS30000.sl2.bak`
+
+Like so: `5_2_2016_10_03__DS30000.sl2.bak`
+
+To restore a backup, delete your `DS30000.sl2` file and rename the backup of your choice to `DS30000.sl2`.
+
+Please note that you will start the backup process by pressing the "Start" button. Then start up Dark Souls 3 and play. When you are done playing, do __not__ forget to press the Stop button or close the DarkSouls3SaveGameBackupTool program. Otherwise, it will continually copy every time interval until stopped.
+
+### [Download the latest release - v2](https://github.com/insane0hflex/DarkSouls3SaveGameBackupTool/blob/master/Releases/DarkSouls3SaveGameBackupTool.exe?raw=true)
 
 __NOTE:__ Upon launching this program, if you are on Windows 10 - you might get a "security" warning. Press the "More info" button then press "Run anyway".
 
-[Virus Total for v1 release](https://www.virustotal.com/en/file/69fefd118f30edc858810287587a849eac9cba94c9772c03959d753540f377d7/analysis/1462063233/)
+[VirusTotal report for DarkSouls3SaveGameBackupTool](https://www.virustotal.com/en/file/69fefd118f30edc858810287587a849eac9cba94c9772c03959d753540f377d7/analysis/1462063233/)
 
-Qihoo360 is a bad AntiVirus with lots of false positives. That AV seems to dislike in particular any C#/.NET 4.0+ project or something.
+Please ignore the Qihoo360 false positive - this antivirus seems to dislike any C#/.NET 4.0+ project or something, which this program was created with.
 
 ![DarkSouls3SaveGameBackupTool](https://github.com/insane0hflex/DarkSouls3SaveGameBackupTool/blob/master/exampleImage.jpg)
 
-ToDos:
+### ToDos
 - ~~Store TimeInterval setting for user~~
 - ~~Make back up file name built with a human readable date rather than .ToFileTime()~~
 - Prettify the UI
-- Better code comments
+- ~~Better code comments~~
 - Dark souls 2 support? 
+- Keep a "MaxBackup" amount - like 10 - where older backups above 10 are automatically deleted.
+- Create a check for the `.exe.config` file and create it if it doesn't exist
+
+### Build Instructions
+
+Want to build the source for this?
+
+Download the source then simply open the solution file (you will need Visual Studio installed, at least .NET Framework 4.0 installed) and build. You can get [Visual Studio Community](https://www.visualstudio.com/en-us/visual-studio-homepage-vs.aspx) for free.
